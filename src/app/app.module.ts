998a31404e0f5aa44810e2950e897aaa18172d84
Park, Yunha <hadbsgk@gmail.com>
2018-05-03 18:12:24 +0900

    Add basic template, routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes} from "@angular/router";
+import { StudycardNavComponent } from './studycard-nav/studycard-nav.component';
+import { StudycardLoginComponent } from './studycard-login/studycard-login.component';
+import { StudycardHomeComponent } from './studycard-home/studycard-home.component';
 
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { environment } from '../environments/environment';
+import { AuthService } from './shared/auth.service';
+
+const appRoutes: Routes = [
+  { path: 'home', component: StudycardHomeComponent},
+  { path: 'login', component: StudycardLoginComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudycardNavComponent,
+    StudycardHomeComponent,    
+    StudycardLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false,
+          useHash: true }
+    ),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
