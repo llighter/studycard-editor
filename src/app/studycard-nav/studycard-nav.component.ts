dfcbbd1bc4c68db9afd6cdca24ba0146d72020ce
Park, Yunha <hadbsgk@gmail.com>
2018-05-03 18:13:29 +0900

    Implement login in nav

--- src/app/studycard-nav/studycard-nav.component.ts
+++ src/app/studycard-nav/studycard-nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as firebase from 'firebase/app';
+import { AuthService } from '../shared/auth.service';
+
 @Component({
   selector: 'app-studycard-nav',
   templateUrl: './studycard-nav.component.html',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudycardNavComponent implements OnInit {
 
-  constructor() { }
+  isLoggedin: boolean;
+  userName: string = 'Not Logged In...';
+
+  constructor(public _auth: AuthService) { }
 
   ngOnInit() {
+    this._auth.user.subscribe((user: firebase.User) => {
+      if(user) {
+        this.userName = user.displayName;
+        this.isLoggedin = true;
+      } else {
+        this.userName = 'Not Logged In';
+      }
+    });
+  }
+
+  logout() {
+    alert("logout");
+    this.isLoggedin = false;
+    this._auth.logout();
   }
 
 }
