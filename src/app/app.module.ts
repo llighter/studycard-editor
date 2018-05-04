66822d808604dede55c0df03b4118b117687c269
Park, Yunha <hadbsgk@gmail.com>
2018-05-04 22:12:00 +0900

    Add home compoenet(material)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes} from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { StudycardNavComponent } from './studycard-nav/studycard-nav.component';
 import { StudycardLoginComponent } from './studycard-login/studycard-login.component';
 import { StudycardHomeComponent } from './studycard-home/studycard-home.component';
 
 import { AngularFireModule } from "angularfire2";
+import { AuthService } from './shared/auth.service';
+import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from "angularfire2/auth";
 import { AngularFirestoreModule } from "angularfire2/firestore";
 import { AngularFireDatabaseModule } from "angularfire2/database";
-import { environment } from '../environments/environment';
-import { AuthService } from './shared/auth.service';
+
+import { MatInputModule } from "@angular/material/input";
+import { MatTableModule } from "@angular/material/table";
+import { MatSelectModule } from "@angular/material/select";
+import { MatPaginatorModule } from "@angular/material/paginator";
+
 
 const appRoutes: Routes = [
   { path: 'home', component: StudycardHomeComponent},
@@ -41,7 +48,11 @@ const appRoutes: Routes = [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    AngularFireDatabaseModule
+    MatInputModule,
+    MatTableModule,
+    MatSelectModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/app/studycard-home/studycard-home.component.ts
+++ src/app/studycard-home/studycard-home.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from "rxjs/Observable";
+
+import { Card } from '../domain/card';
+import { Category } from '../domain/category';
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTableDataSource } from '@angular/material/table';
+
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-studycard-home',
@@ -7,9 +17,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudycardHomeComponent implements OnInit {
 
-  constructor() { }
+  private cardsCollection:AngularFirestoreCollection<Card>;
+  private categoriesCollection: AngularFirestoreCollection<Category>;
+  
+  myCategoryId: string;
+  categories: Observable<Category[]>;
+
+  displayedColumns = ['question', 'answer', 'source', 'repetition', 'stage', 'modifiedDate'];
+  dataSource = new MatTableDataSource<Card>();
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private _auth: AuthService
+    , private afs: AngularFirestore) {
+    const firestore = firebase.firestore();
+    const settings = {/* your settings... */ timestampsInSnapshots: true };
+    firestore.settings(settings);
+  }
 
   ngOnInit() {
+    this._auth.user.subscribe((user: firebase.User) => {
+      if (user) {
+        this.categoriesCollection = this.afs.collection<Category>(`users/${user.uid}/categories`);
+        this.categories = this.categoriesCollection.valueChanges();
+      }
+    })
+  }
+
+  onSelectCategory() {
+    alert(this.myCategoryId);
+
+    this.cardsCollection = this.categoriesCollection
+      .doc(this.myCategoryId)
+      .collection('cardDeck');
+    this.cardsCollection.valueChanges().forEach((doc: Card[]) => {
+      // this.dataSource = new MatTableDataSource<Card>(doc);
+      this.dataSource.data = doc;
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
   }
 
 }
